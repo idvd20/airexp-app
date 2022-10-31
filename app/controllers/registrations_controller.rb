96673bac8221df6f98d99d3e4c4ded87426836ca
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-11-01 01:51:26 +0800

    created a register button on the experience show page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,31 @@
 class RegistrationsController < ApplicationController
 
+    before_action :set_experience 
+
     def index
-        @experience = Experience.find(params[:experience_id])
         @registrations = @experience.registrations
     end
+
+    def new
+        @registration = @experience.registrations.new
+    end
+    
+    def create
+        @registration = @experience.registrations.new(registration_params)
+
+        if @registration.save
+            redirect_to experience_registrations_url(@experience), notice: "Thank for registering!"
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+        def set_experience
+            @experience = Experience.find(params[:experience_id])
+        end
+
+        def registration_params
+            params.require(:registration).permit(:name, :email, :stars)
+        end
 end
