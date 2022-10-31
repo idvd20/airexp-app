bc9bec22899cf8c4ab986f791bd2d5f2cc620e2b
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-11-01 00:17:14 +0800

    styled stars

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -5,4 +5,8 @@ class Registration < ApplicationRecord
   validates :email, format: {with: /\S+@\S+/}
   STARS = (1..5).to_a
   validates :stars, inclusion: {in: STARS, message: "must be between 1 and 5"} 
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
