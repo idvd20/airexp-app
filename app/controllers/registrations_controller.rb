d447ca7bb26d941f10e670ec0689b3efc57edab5
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-10-31 23:23:07 +0800

    nested regisrations route to experience and added registrations index controller and index view

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,7 @@
 class RegistrationsController < ApplicationController
+
+    def index
+        @experience = Experience.find(params[:experience_id])
+        @registrations = @experience.registrations
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :registrations
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  
   root "experiences#index"
-
-  resources :experiences
+  
+  resources :experiences do
+    resources :registrations
+  end
 end
