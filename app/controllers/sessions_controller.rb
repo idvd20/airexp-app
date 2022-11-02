2fbbb7c2a4a0806a725c0001c2b30cd1e695a26d
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-11-03 03:21:47 +0800

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,14 @@ class SessionsController < ApplicationController
     end
 
     def create
-
+        user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user, notice: "Welcome back #{user.username}!"
+        else
+            flash.now[:alert] = "Invalid email/password combination!"
+            render :new, status: :unprocessable_entity
+        end
     end
 
     def destroy
