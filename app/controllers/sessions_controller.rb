a65c174ced44b6fe8ae9decc3b1e8965675756b3
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-11-03 21:52:55 +0800

    authorizations: ensuring the correct user is signed in by creating require correct user before action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
         user = User.find_by(email: params[:email_or_username]) || User.find_by(username: params[:email_or_username])
         if user && user.authenticate(params[:password])
             session[:user_id] = user.id
-            redirect_to user, notice: "Welcome back #{user.username}!"
+            redirect_to (session[:intended_url] || user), notice: "Welcome back #{user.username}!"
+            session[:intended_url] = nil
         else
             flash.now[:alert] = "Invalid email/password combination!"
             render :new, status: :unprocessable_entity
