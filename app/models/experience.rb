3a09de23c1cc87d0757acd80f529fb24200513e6
idvd20 <65553817+idvd20@users.noreply.github.com>
2022-11-11 01:53:59 +0800

    configured active storage and amazon s3

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -45,14 +45,14 @@ class Experience < ApplicationRecord
 
         def acceptable_image
             return unless main_image.attached?
-
-            unless main_image.blob.byte_size <= 5.megabyte
-                errors.add(:main_image, "is too big")
+          
+            unless main_image.blob.byte_size <= 1.megabyte
+              errors.add(:main_image, "is too big")
             end
-
-            acceptable_types = ["image/jpeg", "image/jpg", "image/png"]
+          
+            acceptable_types = ["image/jpeg", "image/png"]
             unless acceptable_types.include?(main_image.content_type)
-                errors.add(:main_image, "must be a JPEG or PNG")
+              errors.add(:main_image, "must be a JPEG or PNG")
             end
         end
 end
